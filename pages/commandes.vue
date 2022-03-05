5f75c0e45d776e9650980ba81825b10dcc1363ff
locheikh <lo.cheikh92110@gmail.com>
2022-03-05 17:57:24 +0100

    first-commit

---
+++ pages/commandes.vue
@@ -0,0 +1,54 @@
+<template>
+<div>
+ <Navbar/>
+    <div class="container">
+        <h1 class="display-1">
+           test
+           <Test/>
+        </h1>
+     <ul id="example-1">
+  <li v-for="produit in produits" :key="produit.name">
+    {{ produit.name }}   {{produit.price}}
+  </li>
+</ul>
+         <Produit  v-for="produit in produits"
+  v-bind:price="produit.price"
+   v-bind:name="produit.name"
+  v-bind:key="produit.name"/>
+    </div>
+</div>
+</template>
+
+<script>
+/* import Produit from '@/components/Produit.vue' */
+export default {
+    
+  data(){
+      return {
+        produits:[{
+            name: 'pizza', price: 2.00
+        },{
+            name: 'choux', price: 3.00
+        }]
+      }
+  },
+
+async mounted() {
+  this.getIncidents ();
+},
+methods:{
+  async getIncidents(){
+    const ip = await this.$axios.$get('http://localhost:3001/api/users')
+    this.ip = ip
+    console.log(ip)
+  }
+}
+
+
+}
+
+</script>
+
+<style scopted>
+
+</style>
